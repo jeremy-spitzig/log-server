e2dfd53dde752d4007dcb78e7b972bff87241827
jeremy-spitzig <jpspitzig@gmail.com>
2021-01-07 19:47:33 -0300

    feat: add class for managing log files

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const LogManager = require('./services/log-manager')
 const app = express()
 
 const PORT = process.env.PORT || 80
+const LOG_DIRECTORY = process.env.LOG_DIRECTORY || '/var/log'
 
-app.get("/", (req, res) => {
-  res.send('Up and running!')
+const logManager = new LogManager(LOG_DIRECTORY)
+
+app.use(express.json())
+app.get('/', (req, res) => {
+  logManager.openFile('install.log')
+    .then(logFile => {
+      logFile.readAll().pipe(res)
+    },
+    error => console.error(error))
 })
 
 app.listen(PORT)
---
+++ services/log-manager.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+
+const fsp = fs.promises
+
+module.exports = class LogManager {
+  constructor(logDirectory, bufferSize) {
+    this.logDirectory = logDirectory
+    this.bufferSize = bufferSize
+  }
+
+  openFile(relativePath) {
+    const fullPath = path.join(this.logDirectory, relativePath)
+    const relative = path.relative(this.logDirectory, fullPath)
+    if(!relative || relative.startsWith('..') || path.isAbsolute(relative)) {
+      return Promise.reject(new Error('File must be within log folder.'))
+    }
+    return fsp.access(fullPath, fs.constants.F_OK | fs.constants.R_OK)
+      .then(() => fsp.stat(fullPath))
+      .then(stat => {
+        if(!stat.isFile()) {
+          throw new Error('File must be a normal file.')
+        }
+        return new LogFile(fullPath)
+      })
+  }
+}
+
+class LogFile {
+  constructor(filePath, bufferSize) {
+    this.bufferSize = bufferSize
+    this.filePath = filePath
+  }
+  readAll() {
+    return fs.createReadStream(this.filePath)
+  }
+}
